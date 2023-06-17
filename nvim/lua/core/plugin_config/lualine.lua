8aaf5ce3863ea9a3f7ed8b14a0f4a4ab50b38aa8
drmaloy <drmaloy85@gmail.com>
2023-06-17 09:54:51 -0500

    Versión modificada de base de los plugins de nvim

--- nvim/lua/core/plugin_config/lualine.lua
+++ nvim/lua/core/plugin_config/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enable = true,
-    theme = 'catppuccin',
+    theme = 'catppuccin', -- gruvbox, dracula, tokyonight
   },
   sections = {
     lualine_a = {
