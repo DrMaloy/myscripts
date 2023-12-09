f6200b5f013d8c524f7f2e1e394d0acf9f20cf28
drmaloy <drmaloy85@gmail.com>
2023-12-09 13:58:00 -0500

    avanzando en js

--- java-script/js_course/js_exer/script.js
+++ java-script/js_course/js_exer/script.js
@@ -144,7 +144,6 @@ if(height) {
   console.log('Height is UNDEFINED');
 }
 
-*/
 
 const age = 18;
 if (age === 18) console.log('You just became an adult :D');
@@ -172,8 +171,31 @@ if (favourite === 23) {
 }
 
 
+*/
 
+const day = 'monday';
 
-
+switch(day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record Videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
 
 
